9edf8bf400375428b79bc9ce916136b524d26ca0
AugustoKopach <143036368+AugustoKopach@users.noreply.github.com>
2023-10-11 22:02:30 -0300

    Add files via upload

---
+++ LaFamilia_mvc/LaFamilia_mvc/src/main/java/TP/LaFamilia_mvc/LaFamiliaMvcApplication.java
@@ -0,0 +1,13 @@
+package TP.LaFamilia_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFamiliaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFamiliaMvcApplication.class, args);
+	}
+
+}
