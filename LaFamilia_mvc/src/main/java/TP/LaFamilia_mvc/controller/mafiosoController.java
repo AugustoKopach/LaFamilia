22e504d260a7cce0378a4337dd36564077d60df2
Augusto <akopach05@gmail.com>
2023-10-11 21:33:25 -0300

    patada inicial

---
+++ LaFamilia_mvc/src/main/java/TP/LaFamilia_mvc/controller/mafiosoController.java
@@ -0,0 +1,9 @@
+package TP.LaFamilia_mvc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mafiosoController {
+
+	
+}
---
+++ LaFamilia_mvc/src/test/java/TP/LaFamilia_mvc/LaFamiliaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package TP.LaFamilia_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaFamiliaMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
